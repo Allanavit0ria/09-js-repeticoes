d5fb10ee64c2dd34f635e2e5626a02c134d377e7
Allanavit0ria <132669005+Allanavit0ria@users.noreply.github.com>
2023-07-07 10:47:45 +0000

    Script.js

--- script.js
+++ script.js
@@ -5,12 +5,28 @@ function contar() {
     let res = document.getElementById('res') 
 
     if (ini.value.length ==0 || fim.value.length ==0 || passo.value.length ==0){
-        window.alert('[ERROR] faltam dados!')
+        res.innerHTML = 'Imposssivel contar!'
+        //window.alert('[ERROR] faltam dados!')
     } else {
         res.innerHTML = 'Contando:'
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p =  Number(passo.value)
-        if(i < f) {
-            
+        if(p <= 0) {
+           window.alert('Passo inválido! Considerando PASSO 1') 
+           p = 1
         }
+        if (i < f) {
+           //Contagem  crescente
+           for(let c = i; c <= f; c += p) {
+            res.innerHTML += `${c} \u{1F449}`
+           }
+        } else {
+            // Contagem regressiva
+            for(let c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
+    }
+}
